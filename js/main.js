b43782a7ea5faadbdfaa7168b3cf777c088f557f
mostafaAboelhadid <mostafaaboelhadid61@gmail.com>
2023-03-08 18:25:28 +0200

    add navbar motion

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
+function closeNav() {
+    $("#nav-bar").width("0px");
+    $(".main").css("margin-left", "0px");
+}
 
+$('.main span').click( function(){
+    if ($("#nav-bar").width() === 0) {
 
+        $('.main').css({"marginLeft": "200px"});
+        $('#nav-bar').width("200px");
 
+    } else {
+        closeNav();
+    }
+});
 
+$('#nav-bar icon').click(closeNav);
 
-
-
-
-
-
-
-
-
-
-$(document).ready(function () {
+$(document).ready(function (){
     $(".singers article:first").css("display", "block");
-    $('.singers h3').click(function () {
+    $('.singers h3').click(function (){
+
         $(this).next().slideToggle(500);
         $(".singers article").not($(this).next()).slideUp(500);
     });
-
-
-})
+});
