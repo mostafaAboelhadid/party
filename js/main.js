6904d4d2064440d5608b874c3cdcc89c4ecb169a
mostafaAboelhadid <mostafaaboelhadid61@gmail.com>
2023-03-08 18:51:47 +0200

    cheker

--- js/main.js
+++ js/main.js
@@ -1,42 +1,39 @@
 // navbar
 
 function closeNav() {
-    $("#nav-bar").width("0px");
-    $(".main").css("margin-left", "0px");
+  $('#nav-bar').width('0px');
+  $('.main').css('margin-left', '0px');
 }
 
-$('.main span').click( function(){
-    if ($("#nav-bar").width() === 0) {
-
-        $('.main').css({"marginLeft": "200px"});
-        $('#nav-bar').width("200px");
-
-    } else {
-        closeNav();
-    }
+$('.main span').click(() => {
+  if ($('#nav-bar').width() === 0) {
+    $('.main').css({ marginLeft: '200px' });
+    $('#nav-bar').width('200px');
+  } else {
+    closeNav();
+  }
 });
 
 $('#nav-bar icon').click(closeNav);
 
 // singers
 
-$(document).ready(function (){
-    $(".singers article:first").css("display", "block");
-    $('.singers h3').click(function (){
-
-        $(this).next().slideToggle(500);
-        $(".singers article").not($(this).next()).slideUp(500);
-    });
+$(document).ready(() => {
+  $('.singers article:first').css('display', 'block');
+  $('.singers h3').click(function () {
+    $(this).next().slideToggle(500);
+    $('.singers article').not($(this).next()).slideUp(500);
+  });
 });
 
 // text area
 
-    $("textarea").keydown(function() {
-        const length = $(this).val().length;
-        const character = 100 - length;
-        if (character <= 0) {
-            $("#char").text("your available character finished");
-        } else {
-            $("#char").text(character);
-        }
-    });
+$('textarea').keydown(function () {
+  const { length } = $(this).val();
+  const character = 100 - length;
+  if (character <= 0) {
+    $('#char').text('your available character finished');
+  } else {
+    $('#char').text(character);
+  }
+});
