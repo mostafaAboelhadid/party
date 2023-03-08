0d4b8ee2e589a498cfe6ccdd67620c240aad7357
mostafaAboelhadid <mostafaaboelhadid61@gmail.com>
2023-03-08 20:15:25 +0200

    add realTime conter

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ function closeNav() {
 
 $('.main span').click(() => {
   if ($('#nav-bar').width() === 0) {
-    $('.main').css({ marginLeft: '200px' });
+    $('.main').css({
+      marginLeft: '200px',
+    });
     $('#nav-bar').width('200px');
   } else {
     closeNav();
@@ -26,10 +28,42 @@ $(document).ready(() => {
   });
 });
 
+// countdown
+
+function countdown() {
+  const todayDate = new Date();
+  const eventDate = new Date('2024, 3, 8');
+
+  const now = todayDate.getTime();
+  const eventTime = eventDate.getTime();
+
+  const restTime = eventTime - now;
+
+  let s = Math.floor(restTime / 1000);
+  let m = Math.floor(s / 60);
+  let h = Math.floor(m / 60);
+  let d = Math.floor(h / 24);
+
+  d -= 30;
+  h %= 24;
+  m %= 60;
+  s %= 60;
+
+  $('.days').text(`${d} D`);
+  $('.hours').text(`${h} h`);
+  $('.mins').text(`${m} m`);
+  $('.sec').text(`${s} s`);
+
+  setTimeout(countdown, 1000);
+}
+countdown();
+
 // text area
 
 $('textarea').keydown(function () {
-  const { length } = $(this).val();
+  const {
+    length,
+  } = $(this).val();
   const character = 100 - length;
   if (character <= 0) {
     $('#char').text('your available character finished');
